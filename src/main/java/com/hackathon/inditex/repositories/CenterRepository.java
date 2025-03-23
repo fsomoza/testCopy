fa11e050114e2fb3d5e290bcf655d2e7c21c4199
fsomoza <kikosomotri@gmail.com>
2025-03-23 14:26:27 +0100

    task 2

--- src/main/java/com/hackathon/inditex/repositories/CenterRepository.java
+++ src/main/java/com/hackathon/inditex/repositories/CenterRepository.java
@@ -1,13 +1,15 @@
 package com.hackathon.inditex.repositories;
 
 import com.hackathon.inditex.Entities.Center;
-import com.hackathon.inditex.Entities.Coordinates;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CenterRepository extends JpaRepository<Center, Long> {
-    Optional<Center> findByCoordinates(Double latitude, Double longitude);
+    @Query("SELECT c FROM Center c WHERE c.coordinates.latitude = :latitude AND c.coordinates.longitude = :longitude")
+    Optional<Center> findByCoordinates(@Param("latitude") Double latitude, @Param("longitude") Double longitude);
 }
